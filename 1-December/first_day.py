8a99357900a6dfd3db13ac99958b545270b9ad90
benedekaibas <kaibas.benedek02@gmail.com>
2024-12-02 10:30:59 -0500

    started the first day

---
+++ 1-December/first_day.py
@@ -0,0 +1,14 @@
+"""First day of Code of Advent."""
+
+from typing import List
+
+
+class AdventOfCode():
+    """Class for containing the necessary functions."""
+   
+    def __init__(self, path):
+        self.path = path
+
+    def open_file(self):
+        """Open the input file that contains the IDs."""
+        
